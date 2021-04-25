d68e5402ac6b86edf4522a35572ee02be83fc52d
Ddogwildone1 <dallinhoyer@gmail.com>
2021-04-25 14:35:07 -0600

    Fixed mongo stuff, so now it publishes to the online mongo one

--- Alwaysrunning/mongo.js
+++ Alwaysrunning/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
-const { mongoPath } = require('../config.json')
+const { dbURI } = require('../config.json')
 
 module.exports = async () => {
-    await mongoose.connect(mongoPath, {
+    await mongoose.connect(dbURI, {
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useFindAndModify: false
